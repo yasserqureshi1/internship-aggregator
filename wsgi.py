523f706395820d64038cdd3d0c380ecba269ad6f
Yasser Qureshi <yasser.q@hotmail.com>
2020-12-20 01:24:58 +0000

    Introduced error handlers

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,24 @@
 from app import create_app
+from flask import render_template
 
 app = create_app()
+app.app_context().push()
+
+
+@app.errorhandler(404)
+def error_404(error):
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(405)
+def error_405(error):
+    return render_template('errors/405.html'), 405
+
+
+@app.errorhandler(500)
+def error_500(error):
+    return render_template('errors/500.html'), 500
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(use_reloader=False)     # Change this
