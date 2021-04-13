1820d7fe20c7427b314a9979fcf26394a68e6537
Yasser Qureshi <yasser.q@hotmail.com>
2021-04-13 02:27:52 +0100

    Added industry to Companies

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from datetime import datetime
 class Companies(db.Model):
    id = db.Column(db.Integer, primary_key=True)
    name = db.Column(db.String(120), nullable=False, unique=True)
+   industry = db.Column(db.String(80), nullable=False)
    description = db.Column(db.Text, nullable=False)
    url = db.Column(db.Text, nullable=False, unique=True)
    positions = db.relationship('Positions', lazy=True, backref='Companies')
@@ -18,6 +19,6 @@ class Positions(db.Model):
    location = db.Column(db.String(120), nullable=False)
    job_type = db.Column(db.String(80), nullable=False)        # [categorical] Internship, graduate, etc.
    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.now)
-   date_closing = db.Column(db.DateTime, nullable=False)
+   date_closing = db.Column(db.DateTime, nullable=True)
    company_id = db.Column(db.Integer, db.ForeignKey('companies.id'), nullable=False)
    url = db.Column(db.Text, nullable=False)
