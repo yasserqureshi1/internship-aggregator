68c62bd765028ddfdcb4882191e52148c025729d
Yasser Qureshi <yasser.q@hotmail.com>
2021-04-18 03:19:26 +0100

    Fix proxy format

--- app/webscraper.py
+++ app/webscraper.py
@@ -120,7 +120,7 @@ class Scraper:
 
         url = 'https://jobs.citi.com/search-jobs/results?ActiveFacetID=Recent+Graduates&CurrentPage=1&RecordsPerPage=500&Distance=50&RadiusUnitType=0&Keywords=&Location=&ShowRadius=False&IsPagination=False&CustomFacetName=&FacetTerm=&FacetType=0&FacetFilters%5B0%5D.ID=Recent+Graduates&FacetFilters%5B0%5D.FacetType=5&FacetFilters%5B0%5D.Count=205&FacetFilters%5B0%5D.Display=Recent+Graduates&FacetFilters%5B0%5D.IsApplied=true&FacetFilters%5B0%5D.FieldName=custom_fields.ECAMPUS&SearchResultsModuleName=Search+Results&SearchFiltersModuleName=Search+Filters&SortCriteria=0&SortDirection=0&SearchType=5&PostalCode=&fc=&fl=&fcf=&afc=&afl=&afcf='
 
-        html = requests.get(url=url, headers=headers, proxies=proxyObject.get())
+        html = requests.get(url=url, headers=headers, proxies={"http": f"http://{proxyObject.get()}"})
         output = json.loads(html.text)
         soup = BeautifulSoup(output['results'], 'html.parser')
 
@@ -146,7 +146,7 @@ class Scraper:
 
         url = 'https://www.goldmansachs.com/careers/students/programs/programs-list.json'
 
-        html = requests.get(url=url, headers={'User-Agent': user_agent_rotator.get_random_user_agent()}, proxies=proxyObject.get())
+        html = requests.get(url=url, headers={'User-Agent': user_agent_rotator.get_random_user_agent()}, proxies={"http": f"http://{proxyObject.get()}"})
         output = json.loads(html.text)
 
         for i in output['programs']:
@@ -168,7 +168,7 @@ class Scraper:
 
         url = 'https://careers.jpmorgan.com/services/json/careers/gate/programs.json'
 
-        html = requests.get(url=url, headers={'User-Agent': user_agent_rotator.get_random_user_agent()}, proxies=proxyObject.get())
+        html = requests.get(url=url, headers={'User-Agent': user_agent_rotator.get_random_user_agent()}, proxies={"http": f"http://{proxyObject.get()}"})
         output = json.loads(html.text)
 
         for i in output:
@@ -193,7 +193,7 @@ class Scraper:
         url = 'https://morganstanley.tal.net/vx/lang-en-GB/mobile-0/brand-2/xf-3786f0ce9359/candidate/jobboard/vacancy/1' \
               '/adv/?f_Item_Opportunity_13857_lk=765&f_Item_Opportunity_17058_lk=133794&ftq= '
 
-        html = requests.get(url=url, headers={'User-Agent': user_agent_rotator.get_random_user_agent()}, proxies=proxyObject.get())
+        html = requests.get(url=url, headers={'User-Agent': user_agent_rotator.get_random_user_agent()}, proxies={"http": f"http://{proxyObject.get()}"})
         output = BeautifulSoup(html.text, 'html.parser')
 
         job_list = output.select('tr[class*="search_res details_row"]')
@@ -217,7 +217,7 @@ class Scraper:
         url = 'https://nomuracampus.tal.net/vx/lang-en-GB/mobile-0/appcentre-ext/brand-4/xf-5b60ed84bdc9/candidate' \
               '/jobboard/vacancy/1/adv/?f_Item_Opportunity_84825_lk=765&ftq='
 
-        html = requests.get(url=url, headers={'User-Agent': user_agent_rotator.get_random_user_agent()}, proxies=proxyObject.get())
+        html = requests.get(url=url, headers={'User-Agent': user_agent_rotator.get_random_user_agent()}, proxies={"http": f"http://{proxyObject.get()}"})
         output = BeautifulSoup(html.text, 'html.parser')
 
         job_list = output.select('tr[class*="search_res details_row"]')
